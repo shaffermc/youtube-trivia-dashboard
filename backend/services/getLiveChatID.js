de09b4b0359220f7070d8e9d8d97a90a51fc437c
litenby <michaelshaffer@protonmail.com>
2026-01-01 10:45:37 -0500

    Fixed directory path to clint_secrets.json

--- backend/services/getLiveChatID.js
+++ backend/services/getLiveChatID.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { google } = require("googleapis");
 
 async function main() {
-  const credentialsPath = path.join(__dirname, "client_secrets.json");
+  const credentialsPath = path.join(__dirname, "..", "client_secrets.json");
   const creds = JSON.parse(fs.readFileSync(credentialsPath, "utf8"));
 
   const { client_id, client_secret, redirect_uris } = creds.installed;
